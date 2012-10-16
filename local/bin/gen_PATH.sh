d0f4f7956f5718b8b170f2f1447bc4970972fa80
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-10-16 17:13:46 +0200

    [local.bin.gen_PATH] also generate path.py

--- local/bin/gen_PATH.sh
+++ local/bin/gen_PATH.sh
@@ -9,3 +9,11 @@ echo "export MY_CC=gcc-4.6" >> $BASEDIR/PATH
 echo "export MY_CXX=g++-4.6" >> $BASEDIR/PATH
 echo "export MY_F77=gfortran-4.6" >> $BASEDIR/PATH
 echo "export MY_CXXFLAGS='-std=c++0x -O0 -g -ggdb -DDEBUG'" >> $BASEDIR/PATH
+
+echo "PATH = \"$BASEDIR/local/bin:$PATH\"" > $BASEDIR/path.py
+echo "LD_LIBRARY_PATH = \"$BASEDIR/local/lib:$LD_LIBRARY_PATH\"" >> $BASEDIR/path.py
+echo "PKG_CONFIG_PATH = \"$BASEDIR/local/lib/pkgconfig:$PKG_CONFIG_PATH\"" >> $BASEDIR/path.py
+echo "MY_CC = \"gcc-4.6\"" >> $BASEDIR/path.py
+echo "MY_CXX = \"g++-4.6\"" >> $BASEDIR/path.py
+echo "MY_F77 = \"gfortran-4.6\"" >> $BASEDIR/path.py
+echo "MY_CXXFLAGS = \"-std=c++0x -O0 -g -ggdb -DDEBUG\"" >> $BASEDIR/path.py
