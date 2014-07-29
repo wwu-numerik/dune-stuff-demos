99ca19da97cb22b6acd6f3ba77d2f7f73b4167a4
Rene Milk <rene.milk@uni-muenster.de>
2014-07-29 17:09:19 +0200

    bump

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 set -e
 . dune_utils.bash
 getOptsFile $1
-./dune-common/bin/dunecontrol --opts=${1} all
+./dune-common/bin/dunecontrol --opts=${1} --use-cmake all
--- setup_module.sh
+++ setup_module.sh
@@ -2,4 +2,4 @@
 set -e
 . dune_utils.bash
 getOptsFile $2
-./dune-common/bin/dunecontrol --module=$1 --opts=${OPTS} all
+./dune-common/bin/dunecontrol --module=$1 --opts=${OPTS} --use-cmake all
